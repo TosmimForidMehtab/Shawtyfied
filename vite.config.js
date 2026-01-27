c4fefd91e3bf0689f9ff0d3b5f88dfbdf7d04622
Tosmim <tosmimforidmehtab@gmail.com>
2026-01-28 02:00:12 +0530

    Added PWA Support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate', includeAssets: ['fav.png', 'robots.txt', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'Shawtyfied',
+      short_name: 'Shawtyfied',
+      description: 'Shawtyfied is a URL shortening service that allows you to shorten long URLs and share them with ease.',
+      theme_color: '#0c0c1d',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable'
+        }
+      ]
+    }
+  })],
 })
